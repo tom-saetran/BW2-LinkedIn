61ea1833ca9e05a4deea264a2b47c83a33edf8d1
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-05-10 13:44:36 +0200

    changed stuff!

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,20 +1,26 @@
 import React from "react"
-import { Container, Row, Col} from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
 import ProfileJumbotron from "./ProfileJumbotron"
 
-
 class Profile extends React.Component {
+    state = {
+        me: {},
+        all: {}
+    }
+
+    componentDidMount = async () => {
+        this.setState({ me: await this.props.me })
+        this.setState({ all: await this.props.all })
+    }
 
     render() {
         return (
             <Container>
                 <Row>
                     <Col md={8}>
-                        <ProfileJumbotron/>
+                        <ProfileJumbotron me={this.state.me} />
                     </Col>
-                    <Col md={4}>
-                        {/* */}
-                    </Col>
+                    <Col md={4}>{/* */}</Col>
                 </Row>
             </Container>
         )
