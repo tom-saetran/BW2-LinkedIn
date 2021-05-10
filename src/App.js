cb9470e72d0cf8d80b07b3cdbadd4a131e9f5574
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-05-10 09:39:17 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom/"
+import HTTP501 from "./components/HTTP501"
+import HTTP404 from "./components/HTTP404"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route render={routerProps => <HTTP501 {...routerProps} />} exact path="/" />
+                <Route render={routerProps => <HTTP404 {...routerProps} />} />
+            </Switch>
+        </Router>
+    )
 }
 
-export default App;
+export default App
