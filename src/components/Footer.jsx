204453b31b7612c75f23762a350777ee9e754d5d
D4WKINS <78683271+D4WKINS@users.noreply.github.com>
2021-05-11 17:59:11 +0100

    Update Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+    import {Container} from 'react-bootstrap'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <>
+                <Footer className="d-flex">
+                    <Container>
+                    <ul className="col-lg-2">
+                        <li>Yes</li>
+                        <li>Brav</li>
+                        <li>Lets</li>
+                        <li>get</li>
+                        <li>it </li>
+                        <li>Going</li>
+                    </ul>
+
+                    <ul className="col-lg-2">
+                        <li>Noice </li>
+                        <li>one </li>
+                        <li>brother</li>
+                        <li>....</li>
+                    </ul>
+
+                    <ul  className="col-lg-2">
+                        <li>I</li>
+                        <li>saidd</li>
+                        <li>Nice</li>
+                        <li>One</li>
+                    </ul>
+
+                    <ul className="col-lg-2">
+                        <li>Brav</li>
+                        <li>ahahahaha</li>
+                    </ul>
+
+                     <ul className="col-lg-2">
+                        <li>hahahahahhaa</li>
+                        <li>hehhehhehe</li>
+                    </ul>
+
+                    </Container>
+                </Footer>
+            </>
+        )
+    }
+}
