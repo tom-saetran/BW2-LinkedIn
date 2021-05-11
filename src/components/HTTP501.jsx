5fe3f61360ad24fad4dee58a0c09a0ce4e58c6bb
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-05-11 10:33:52 +0200

    merge yay

--- src/components/HTTP501.jsx
+++ src/components/HTTP501.jsx
@@ -1,8 +1,21 @@
 import React from "react"
+import { Link } from "react-router-dom"
+import "../assets/toms-bootstrap-extensions.css"
 
 class HTTP501 extends React.Component {
     render() {
-        return <div>Hello World!</div>
+        return (
+            <>
+                <div className="text-dim text-center">
+                    <h1>501 - Not Implemented</h1>
+                    <Link className="no-underline" to="/profile">
+                        <h3 className="link-dim">
+                            Go to <code className="text-success">/profile</code>
+                        </h3>
+                    </Link>
+                </div>
+            </>
+        )
     }
 }
 
