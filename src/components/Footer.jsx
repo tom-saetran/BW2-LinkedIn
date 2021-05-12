c0f69895fc17dccce63aaa9e44e5b9679efcdac2
kobrakai415 <kai.kadir@hotmail.co.uk>
2021-05-12 15:09:43 +0100

    footer structure set

---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import {Container, Row, Col} from "react-bootstrap"
+
+
+class Footer extends React.Component {
+
+
+    render() {
+
+        return (
+            <Container>
+                <Row>
+                    <Col xs={2}>
+                    <ul className="d-flex flex-column flex-start">
+                    <li>Carl</li>
+                    <li>Carl</li>
+                    <li>Carl</li>
+
+                    
+
+                    </ul>
+
+                    </Col>
+                    <Col xs={2}>
+                        
+                    </Col>
+                    <Col xs={2}>
+                        
+                    </Col>
+                    <Col xs={3}>
+                        
+                    </Col>
+                    <Col xs={3}>
+                        
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default Footer 
