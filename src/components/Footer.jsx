2a21dfd9cc0a558ef552058818d7652e868f66b5
D4WKINS <78683271+D4WKINS@users.noreply.github.com>
2021-05-13 16:45:32 +0100

    Create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,23 +11,42 @@ class Footer extends React.Component {
             <Container>
                 <Row>
                     <Col xs={2}>
-                    <ul className="d-flex flex-column flex-start">
-                    <li>Carl</li>
-                    <li>Carl</li>
-                    <li>Carl</li>
-
-                    
-
+                    <ul className="d-flex flex-column flex-start footer-list">
+                    <li>About</li>
+                    <li>Community Guidelines</li>
+                     <li>Privacy & Terms </li>
+                     <li>Sales Solutions</li>
+                     <li>Safety Center</li>
                     </ul>
 
                     </Col>
                     <Col xs={2}>
+                        <ul className="d-flex flex-column flex-start footer-list">
+                        <li>Accessibilty </li>
+                        <li>Careers</li>
+                        <li>Ad Choices</li>
+                        <li>Mobile</li>
                         
+                        </ul>
                     </Col>
                     <Col xs={2}>
-                        
+                    <ul className="d-flex flex-column flex-start footer-list">
+                        <li>Talent Solution</li>
+                        <li>Marketing Solutions</li>
+                        <li>Advertising</li>
+                        <li>Small Business</li>
+                        </ul>
                     </Col>
                     <Col xs={3}>
+                    <ul className="d-flex flex-column flex-start footer-list">
+                        <li className="d-flex">
+                            <li>Questions?</li>
+                            <li></li>
+                        </li>
+
+                        <li>Marketing Solutions</li>
+                    </ul>
+
                         
                     </Col>
                     <Col xs={3}>
